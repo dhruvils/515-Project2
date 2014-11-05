fd443ed666633046164866a5589e7d2ec7b27947
Justin Chiu <justc@seas.upenn.edu>
2014-11-05 10:40:41 -0500

    ginput

--- main.m
+++ main.m
@@ -5,8 +5,9 @@ function main()
 
 for i = 1:6
     %deCas_subdivision([1 1], [3 3], [5 5], [7 2], i);
+    deCas_subdivision([x(1), y(1)], [x(2),y(2)], [x(3),y(3)], [x(4),y(4)], i);
     %deCas_subdivision_m([1 1;3 3;5 5;7 2;9 1; 10 3], i, true);
-    deCas_subdivision_m([x,y], i, true);
+    %deCas_subdivision_m([x,y], i, true);
 end
 
 
